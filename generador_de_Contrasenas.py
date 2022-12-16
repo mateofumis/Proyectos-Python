01ce9dc49dfa8c80fe944b725b7e4687fa919150
mateofumis <mateofumis1@gmail.com>
2022-12-15 22:08:50 -0300

    increase symbols density

--- generador_de_Contrasenas.py
+++ generador_de_Contrasenas.py
@@ -3,7 +3,7 @@ import random
 def passwordGenerator(num):
     mayusculas = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     minusculas = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"]
-    simbolos = ["#","$","!","&","-","_","*"]
+    simbolos = ["#","$","!","&","-","_","*","#","$","!","&","-","_","*","#","$","!","&","-","_","*"]
     numeros = ["1","2","3","4","5","6","7","8","9","0","1","2","3","4","5","6","7","8","9","0"]
 
     caracteres = mayusculas +  minusculas + simbolos + numeros
